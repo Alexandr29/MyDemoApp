a7daf736e368aecd1e886f950629ae701375ebf1
Alexander <alexandr290497@gmail.com>
2017-11-02 11:37:07 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/NewController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewController {
+    private final String msg;
+
+    public NewController(@Value("${custom.message}") String msg) {
+        this.msg = msg;
+    }
+
+    @RequestMapping(value = "/index.html", method = RequestMethod.GET)
+    public String index(){
+        return "<html>" +
+                "<body>" +
+                "<h1>"+ msg + "</h1>" +
+                "</body>" +
+                "</html>";
+    }
+}
